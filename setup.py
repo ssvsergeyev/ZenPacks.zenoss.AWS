09f351558129b909e7d79bf7fb074a34de648318
Chet Luther <cluther@zenoss.com>
2013-03-12 17:58:21 -0500

    Update license. Version to 2.0.0dev2.

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@
 # or saved.  Do not modify them directly here.
 # NB: PACKAGES is deprecated
 NAME = "ZenPacks.zenoss.AWS"
-VERSION = "3.0.0dev1"
+VERSION = "2.0.0dev2"
 AUTHOR = "Zenoss"
-LICENSE = "All Rights Reserved"
+LICENSE = "GPLv2"
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.zenoss']
 PACKAGES = ['ZenPacks', 'ZenPacks.zenoss', 'ZenPacks.zenoss.AWS']
 INSTALL_REQUIRES = ['ZenPacks.zenoss.PythonCollector']
