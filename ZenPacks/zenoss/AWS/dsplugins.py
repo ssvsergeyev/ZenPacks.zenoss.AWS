403e380f72dcfdf98d5a62d82070fc451ef2e284
ssirushchyshyn <irushch@softserveinc.com>
2013-12-23 18:39:09 +0200

    Created a base plugin class

--- ZenPacks/zenoss/AWS/dsplugins.py
+++ ZenPacks/zenoss/AWS/dsplugins.py
@@ -23,7 +23,7 @@ from ZenPacks.zenoss.PythonCollector.datasources.PythonDataSource \
     import PythonDataSourcePlugin
 
 
-class S3BucketPlugin(PythonDataSourcePlugin):
+class AWSBasePlugin(PythonDataSourcePlugin):
     """
     Subclass of PythonDataSourcePlugin to monitor AWS S3Buckets.
     """
@@ -33,19 +33,7 @@ class S3BucketPlugin(PythonDataSourcePlugin):
 
     @defer.inlineCallbacks
     def collect(self, config):
-        data = {'events': [], 'values': {}, 'maps': []}
-        for ds in config.datasources:
-            s3connection = S3Connection(ds.ec2accesskey, ds.ec2secretkey)
-            bucket = s3connection.get_bucket(ds.component)
-            keys = yield bucket.get_all_keys()
-
-            t = time.time()
-            data['values'][ds.component] = dict(
-                keys_count=(len(keys), t),
-                total_size=(sum([key.size for key in keys]), t),
-            )
-
-        defer.returnValue(data)
+        pass
 
     def onSuccess(self, result, config):
         for component in result["values"].keys():
@@ -72,7 +60,32 @@ class S3BucketPlugin(PythonDataSourcePlugin):
         }
 
 
-class EC2RegionPlugin(PythonDataSourcePlugin):
+class S3BucketPlugin(AWSBasePlugin):
+    """
+    Subclass of PythonDataSourcePlugin to monitor AWS S3Buckets.
+    """
+    proxy_attributes = (
+        'ec2accesskey', 'ec2secretkey',
+    )
+
+    @defer.inlineCallbacks
+    def collect(self, config):
+        data = {'events': [], 'values': {}, 'maps': []}
+        for ds in config.datasources:
+            s3connection = S3Connection(ds.ec2accesskey, ds.ec2secretkey)
+            bucket = s3connection.get_bucket(ds.component)
+            keys = yield bucket.get_all_keys()
+
+            t = time.time()
+            data['values'][ds.component] = dict(
+                keys_count=(len(keys), t),
+                total_size=(sum([key.size for key in keys]), t),
+            )
+
+        defer.returnValue(data)
+
+
+class EC2RegionPlugin(AWSBasePlugin):
     """
     Subclass of PythonDataSourcePlugin to monitor AWS EC2Region soft limits.
     """
@@ -128,27 +141,3 @@ class EC2RegionPlugin(PythonDataSourcePlugin):
         # print "==" * 20
         # print data
         defer.returnValue(data)
-
-    def onSuccess(self, result, config):
-        for component in result["values"].keys():
-            result['events'].insert(0, {
-                'component': component,
-                'summary': 'Monitoring ok',
-                'eventClass': '/Status',
-                'eventKey': 'aws_result',
-                'severity': 0,
-            })
-        return result
-
-    def onError(self, result, config):
-        log.error(result)
-        return {
-            'vaues': {},
-            'events': [{
-                'summary': 'error: %s' % result,
-                'eventClass': '/Status',
-                'eventKey': 'aws_result',
-                'severity': 4,
-            }],
-            'maps': [],
-        }
