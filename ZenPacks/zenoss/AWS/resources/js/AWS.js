1a6c30149081614448a97e5be98ea43a44a0b2a7
Andrii Martyniuk <amartyn@softserveinc.com>
2013-12-25 11:38:08 +0200

    AWS moddeling UI

--- ZenPacks/zenoss/AWS/resources/js/AWS.js
+++ ZenPacks/zenoss/AWS/resources/js/AWS.js
@@ -18,6 +18,11 @@ ZC.registerName('EC2Volume', _t('Volume'), _t('Volumes'));
 ZC.registerName('EC2VPC', _t('VPC'), _t('VPCs'));
 ZC.registerName('EC2VPCSubnet', _t('Subnet'), _t('Subnets'));
 ZC.registerName('EC2Zone', _t('Zone'), _t('Zones'));
+ZC.registerName('VPNGateway', _t('Gateway'), _t('Gateways'));
+ZC.registerName('EC2Reservation', _t('Reservation'), _t('Reservations'))
+ZC.registerName('S3Bucket', _t('Bucket'), _t('Buckets'))
+ZC.registerName('EC2ElasticIP', _t('ElasticIP'), _t('ElasticIPs'))
+ZC.registerName('EC2Image', _t('Image'), _t('Images'))
 
 
 var add_ec2account = new Zenoss.Action({
